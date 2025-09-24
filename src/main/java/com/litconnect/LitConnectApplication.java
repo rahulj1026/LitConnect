603fef63240b533b1e9c6673da8e80a45427cc0c
rahulj1026 <rahul.vilas.jadhav1999@gmail.com>
2025-09-24 09:02:41 +0530

    Initial commit - project setup

---
+++ src/main/java/com/litconnect/LitConnectApplication.java
@@ -0,0 +1,13 @@
+package com.litconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/litconnect/LitConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
